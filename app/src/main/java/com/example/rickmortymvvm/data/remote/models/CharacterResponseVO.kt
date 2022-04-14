cd74ab04699fb2ede14e9d4af29949aeb066344e
Matheus Carvalho <carvalho2111@gmail.com>
2022-04-14 15:20:17 -0300

    small correction

--- app/src/main/java/com/example/rickmortymvvm/data/remote/models/CharacterResponseVO.kt
+++ app/src/main/java/com/example/rickmortymvvm/data/remote/models/CharacterResponseVO.kt
@@ -3,6 +3,6 @@ package com.example.rickmortymvvm.data.remote.models
 import com.example.rickmortymvvm.models.Character
 
 data class CharacterResponseVO(
-        val info: CharacterResponseInfoVO? = null,
-        val results: List<Character>? = null,
+    val info: CharacterResponseInfoVO? = null,
+    val results: List<Character>? = null,
 )
