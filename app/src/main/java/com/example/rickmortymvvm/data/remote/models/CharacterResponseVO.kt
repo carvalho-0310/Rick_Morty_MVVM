fd12cb451ddd77bb3dc61b5dc435a81c16760e36
Matheus Carvalho <carvalho2111@gmail.com>
2022-04-17 20:21:41 -0300

    add readme

--- app/src/main/java/com/example/rickmortymvvm/data/remote/models/CharacterResponseVO.kt
+++ app/src/main/java/com/example/rickmortymvvm/data/remote/models/CharacterResponseVO.kt
@@ -1,8 +1,11 @@
 package com.example.rickmortymvvm.data.remote.models
 
 import com.example.rickmortymvvm.models.Character
+import com.google.gson.annotations.SerializedName
 
 data class CharacterResponseVO(
+        @SerializedName("info")
     val info: CharacterResponseInfoVO? = null,
+        @SerializedName("results")
     val results: List<Character>? = null,
 )
